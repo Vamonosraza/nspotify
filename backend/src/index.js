50c4818389f05f32ed4969e92cfd068d49661f5c
Vamonosraza <martinezjessy393@gmail.com>
2025-02-16 14:23:49 -0800

    add user model and database connection utility

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.route.js';
 import songRoutes from './routes/songs.route.js';
 import albumsRoutes from './routes/albums.route.js';
 import statisticsRoutes from './routes/statistics.route.js';
+import {connectDB} from "./lib/db.js";
 
 dotenv.config();
 
@@ -21,4 +22,5 @@ app.use('/api/statistics', statisticsRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
+  connectDB();
 });

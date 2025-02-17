858115e1d0e4a1e76b6c2523f32811a73149bca5
Vamonosraza <martinezjessy393@gmail.com>
2025-02-16 16:52:15 -0800

    add fullName and imageUrl fields to user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,12 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-
-}
+    fullName:{
+        type: String,
+        required: true
+    },
+    imageUrl:{
+        type:String,
+        required: true
+    }
+});

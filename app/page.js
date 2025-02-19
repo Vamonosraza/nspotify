d580c269424221d9a5e208e0d9b0bb0825056ab5
Vamonosraza <martinezjessy393@gmail.com>
2025-02-18 17:38:10 -0800

    add search functionality for artists and display their albums in the homepage. (Unauthorized)

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import Link from "next/link";
+'use client';
 import Card from "@/components/Card/card";
 import { useState, useEffect} from "react";
 
@@ -35,17 +34,52 @@ export default function Home() {
                 'Authorization': `Bearer ${accessToken}`
             }
         }
+        const artistID = await fetch(`https://api.spotify.com/v1/search?q=${searchInput}&type=artist`, artistParams)
+            .then(result => result.json())
+            .then(data => {
+              return data.artists.items[0].id;
+            });
+
+          // Get Artist Albums
+        await fetch(`https://api.spotify.com/v1/artists/${artistID}/albums?include_groups=album&market=US&limit=50`, artistParams)
+            .then(result => result.json())
+            .then(data => {
+              setAlbums(data.items);
+            });
     }
+
+
 
   return (
       <>
-      <h1>Homepage</h1>
-      <div className="grid grid-cols-3 gap-4">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+          <h1>Homepage</h1>
+          <div className="mb-4">
+              <input
+                  type="text"
+                  placeholder="Search For Artist"
+                  className="input input-bordered w-full max-w-xs"
+                  onKeyDown={event => {
+                      if (event.key === "Enter") {
+                          search();
+                      }
+                  }}
+                  onChange={event => setSearchInput(event.target.value)}
+              />
+              <button className="btn btn-primary ml-2" onClick={search}>
+                  Search
+              </button>
+          </div>
+          <div className="grid grid-cols-3 gap-4">
+              {albums.map((album) => (
+                  <Card key={album.id} album={album}/>
+              ))}
+          </div>
+          <div className="grid grid-cols-3 gap-4">
+              <Card/>
+              <Card/>
+              <Card/>
+              <Card/>
+          </div>
       </>
   );
 }
